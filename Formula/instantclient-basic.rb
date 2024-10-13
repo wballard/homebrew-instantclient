6eaac231d8156e06e68a52d4edc15bdecf8c8741
Will Ballard <wballard@mailframe.net>
2024-10-13 14:08:36 -0500

    Update instantclient-basic.rb

--- Formula/instantclient-basic.rb
+++ Formula/instantclient-basic.rb
@@ -3,8 +3,8 @@ class InstantclientBasic < Formula
   desc "Oracle Instant Client Basic x64"
   homepage "https://www.oracle.com/database/technologies/instant-client/macos-intel-x86-downloads.html"
 
-  url "https://download.oracle.com/otn_software/mac/instantclient/198000/instantclient-basic-macos.x64-19.8.0.0.0dbru.zip"
-  sha256 "57ed4198f3a10d83cd5ddc2472c058d4c3b0b786246baebf6bbfc7391cc12087"
+  url "https://download.oracle.com/otn_software/mac/instantclient/233023/instantclient-basic-macos.arm64-23.3.0.23.09-1.dmg"
+  #sha256 "57ed4198f3a10d83cd5ddc2472c058d4c3b0b786246baebf6bbfc7391cc12087"
 
   conflicts_with "instantclient-basiclite"
 
